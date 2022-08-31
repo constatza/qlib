20b1f737e5062f4d66e7e6995a3051fa4385d3d0
archer <gitlab.in2mf@simplelogin.co>
2022-08-31 08:53:32 +0300

    added general test

---
+++ lde/test_non_unitary.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 30 15:30:22 2022
+
+@author: archer
+"""
+
+from lde import *
+import matplotlib.pyplot as plt
+import numpy as np
+import qiskit
+
+
+taylor_terms = np.arange(1, 3)
+M = np.array([[1, 1],
+              [-1, 0]])
+
+# M = np.eye(2**num_working_qubits)
+
+# M = np.random.rand(2**num_working_qubits, 2**num_working_qubits)
+
+
+lcu_coeffs = np.array([.5]*4)
+x0 = np.array([1, -1])
+
+
+backend = qiskit.Aer.get_backend('statevector_simulator',
+                                 device='GPU',
+                                 max_parallel_threads=20,
+                                 max_parallel_experiments=20,
+                                 precision="single",
+                                 )
+
+
+time = np.linspace(0.0, 2)
+
+
+solutions = []
+
+for num_terms in taylor_terms:
+    lde = Evolution(M, x0, k=num_terms)
+    lde.evolve(10)
+
+#     sim = RangeSimulation(lde, backend=backend)
+
+#     solutions.append(sim.simulate_range(time))
+
+
+# fig, ax = plt.subplots()
+
+# for i, num_terms in enumerate(taylor_terms):
+#     x = solutions[i]
+#     ax.plot(time, x[:, 0], '--', label=f"k={num_terms:d}")
+
+
+# x_classical = sim.simulate_range_exact(time)
+# ax.plot(time, x_classical[:, 0],  color='g', label="Exact")
+
+
+# ax.set_title("LDE 1-dof system $\ddot{x} + x = 0$")
+# ax.set_ylabel("$x(t)$")
+# ax.set_xlabel('$t$')
+# ax.legend()
+
+
+# # plt.savefig("/home/archer/Documents/phd/presentations/lde/img/solution_per_taylor_terms_1dof_xdot.png",
+# #             dpi=400,
+# #             format='png')
+
+# plt.show()

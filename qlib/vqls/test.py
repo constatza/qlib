c90d76be7c8d2065040504a174afd0e68bb38692
archer <gitlab.in2mf@simplelogin.co>
2022-09-22 18:31:35 +0300

    local cost working

--- qlib/vqls/test.py
+++ qlib/vqls/test.py
@@ -6,7 +6,11 @@ Created on Tue Jul  5 14:56:15 2022
 
 @author: archer
 """
-from vqls import *
+import numpy as np
+from qiskit import Aer
+from vqls import VQLS, FixedAnsatz, LocalProjector, FixedAnsatz
+from qlib.utils import states2qubits
+
 
 backend = Aer.get_backend('statevector_simulator',
                          max_parallel_threads=4,
@@ -17,54 +21,25 @@ backend = Aer.get_backend('statevector_simulator',
 #                                     max_parallel_threads=8,
 #                                     max_parallel_experiments=16,
 #                                     precision="single")
-size = 4
-num_layers = 1
+size = 2**4
+num_layers = 3
 num_shots = 2**11
 
 b = np.ones(size)
 A = np.random.rand(size, size)
-
-
-ansatz = FixedAnsatz(states2qubits(size), num_layers=num_layers)
-parameters0 = np.random.rand(ansatz.num_parameters)
-
-vqls = VQLS(A, b, ansatz)
-
-
 x = np.linalg.solve(A, b)
 
-b_compl = b
-b_compl[::2] *= -1
-# ansatz = library.QAOAAnsatz(cost_operator=Hop, reps=6)
-# draw(ansatz.decompose())
-
-
+vqls = VQLS(A, b)
 
 
 options = {'maxiter': 50,
+           'tol': 1e-4,
     'disp': True}
 
-cons = ({'type': 'ineq', 'fun': lambda x:  x})
-bounds = [(0, 2*np.pi) for i in range(9)]
-print("# Optimizing")
-result = minimize(vqls.local_cost,
-            method='COBYLA',
-            x0=parameters0,
-            options=options,
-            callback=vqls.fun_callback)
-
-
-x_up_to_constant = vqls.optimal_state(result.x)
-b_up_to_constant = x_up_to_constant.dot(A)
-constant = np.mean(b/b_up_to_constant)
-xopt = constant*x_up_to_constant
-bopt = xopt.dot(A)
-
-print("# solution ", x)
-print("# approx", xopt)
+bounds = [(0, 2*np.pi) for _ in range(vqls.ansatz.num_parameters)]
 
 
 
+x_approx = vqls.solve(optimizer='COBYLA',  options=options).get_solution()
 
-
-
+b_approx = x_approx.dot(A)
--- qlib/vqls/vqls.py
+++ qlib/vqls/vqls.py
@@ -12,248 +12,116 @@ from scipy.optimize import minimize
 
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit,\
     transpile, Aer, execute
-import qiskit.circuit.library as library
-from qiskit.quantum_info import Operator
-from qiskit.circuit import ParameterVector, QuantumCircuit
-from qiskit.compiler import transpile, assemble
-from qiskit.opflow import MatrixOp, Zero, PrimitiveOp, CircuitOp, SummedOp, ComposedOp, TensoredOp
-from qlib.utils import normalized, LinearDecompositionOfUnitaries, norm,\
+from qiskit.circuit import ParameterVector
+from qlib.utils import LinearDecompositionOfUnitaries, \
     unitary_from_column_vector, draw, states2qubits
-from qiskit.opflow import Z, X, I
 
 
 backend = Aer.get_backend('statevector_simulator')
 
 
-class VQLS:
-    
-    def __init__(self, A, b, ansatz, backend=backend, num_shots=1):
-        self.matrix = A
-        self.target = b
-        self.lcu = LinearDecompositionOfUnitaries(A)
-        self.Ub = unitary_from_column_vector(b)
-        self.ansatz = ansatz
-        self.projector = LocalProjector(self.lcu, ansatz, self.Ub)
-        self.num_working_qubits = self.projector.num_working_qubits
-        self.backend = backend
-        self.num_shots = num_shots
-        self.circuits = None
-        self.num_unitaries = None
-        self.num_jobs = None
-        self.construct_circuits()
 
-    def construct_circuits(self):
-        num_qubits = self.num_working_qubits + 1
-        num_unitaries = self.lcu.num_unitaries
-        circuits = np.empty((num_qubits, num_unitaries, num_unitaries),
-                            dtype=object)
-        for j in range(num_qubits):
-            for m in range(num_unitaries):
-                for n in range(num_unitaries):
-                    circuits[j, m, n] = self.construct_term(m, n, j=j)
-                    # draw(circuits[j, m, n][1])
-        self.circuits = circuits
-        self.num_jobs = circuits.size*2
-        return self
-    
-    
-    def construct_term(self, mu, nu, j):
-        opt = 3
-   
-        operation = self.projector.get_circuit(mu, nu, j=j)
-        
-        hadamard_real = HadamardTest(operation, imaginary=False).get_circuit()
-        hadamard_imag = HadamardTest(operation, imaginary=True).get_circuit()
-        
-        # transpiled = transpile([hadamard_real, hadamard_imag],
-        #                               backend=backend,
-        #                               optimization_level=opt)
-    
-        # return transpiled
-        return (hadamard_real, hadamard_imag)
-    
-    
-    def run_circuits(self, values):
-        experiments = []
-        
-        for pair in self.circuits.flatten().tolist():
-            for qc in pair:
 
-                experiments.append(qc.assign_parameters(values))
-        # binds = {p: v for (p, v) in zip(qc.parameters, values)}
-        self.job = execute(experiments,
-                           backend=self.backend)
-        
-        return self
-    
-    
-    def get_results(self, between):
-        irange = range(between[0], between[1])
-        
-        results = np.zeros(len(irange), dtype=float)
-        result = self.job.result()
-        
-        if self.backend.name() == 'statevector_simulator':
-            for i in irange:  # 2 parts: real, imag per term
-                state = result.get_statevector(i)
-                results[i-between[0]] = state.probabilities([0])[0]
-        else:
-            for i in irange:  # 2 parts: real, imag per term
-                counts = result.get_counts(i)
-                results[i-between[0]] = counts['0']/self.num_shots
-    
-        return results
-    
-    
-    def local_cost(self, values):
-        """Returns the normalization constant <psi|psi>, where |psi> = A |x>."""
-        num_working_qubits= self.num_working_qubits
-        coeffs = np.atleast_2d(self.lcu.coeffs)
-        L = coeffs.shape[1]
-    
-        # Jobs
-        self.run_circuits(values)
-        num_jobs_beta =2*L**2
-    
-        # Beta results
-        index_jobs_beta = (0, num_jobs_beta)
-        beta_p0 = self.get_results(index_jobs_beta)
-        beta_p0 = 2*beta_p0 - 1
-        betas = beta_p0[:-1:2] + 1j*beta_p0[1::2]
-        betas = betas.reshape((L, L))
-    
-        # delta results
-        num_jobs_delta = num_working_qubits*num_jobs_beta
-        index_jobs_delta = (num_jobs_beta, num_jobs_beta + num_jobs_delta)
-        delta_p0 = self.get_results(index_jobs_delta)
-        delta_p0 = 2*delta_p0 - 1
-        deltas = delta_p0[:-1:2] + 1j*delta_p0[1::2]
-        deltas = deltas.reshape((num_working_qubits, L, L))
-    
-        delta_sum = coeffs.dot(betas + deltas.sum(axis=0)).dot(coeffs.conj().T).real[0, 0]
-        beta_norm = coeffs.dot(betas).dot(coeffs.conj().T).real[0, 0]
-    
-        self.cost = 1 - delta_sum/beta_norm/num_working_qubits/2
-        return self.cost
-    
-    
-    def optimal_state(self, values_opt):
-        backend = Aer.get_backend('statevector_simulator')
-        qc = self.ansatz.get_circuit().assign_parameters(values_opt)
-        job = execute(qc, optimization_level=3,
-                               backend=backend)
 
-        state = job.result().get_statevector()
-    
-        return np.asarray(state).real
-    
-    def fun_callback(self, x):
-        print("{:.5e}".format(self.cost))
-
- 
 class FixedAnsatz:
 
     def __init__(self, num_qubits, num_layers=1):
         self.num_qubits = num_qubits
         self.num_layers = num_layers
-        self.num_parameters = 2*num_layers*(num_qubits//2 + (num_qubits-1)//2) + num_qubits
-        self.alpha = ParameterVector('a', self.num_parameters) 
-        self.qc =  QuantumCircuit(num_qubits, name='V')
+        self.num_parameters = 2*num_layers * \
+            (num_qubits//2 + (num_qubits-1)//2) + num_qubits
+        self.alpha = ParameterVector('a', self.num_parameters)
+        self.qc = QuantumCircuit(num_qubits, name='V')
         self.construct_circuit()
 
-    
     def construct_circuit(self):
         qc = self.qc
         qubits = qc.qubits
         alpha = self.alpha
-    
+
         ia = 0
         for iz in range(self.num_qubits):
             qc.ry(alpha[ia], iz)
             ia += 1
-            
+
         for _ in range(self.num_layers):
             for iz in range(0, self.num_qubits-1, 2):
                 qc.cz(iz, iz+1)
                 qc.ry(alpha[ia], qubits[iz])
                 qc.ry(alpha[ia+1], qubits[iz+1])
                 ia += 2
-    
+
             for iz in range(1, self.num_qubits-1, 2):
                 qc.cz(iz, iz+1)
                 qc.ry(alpha[ia], qubits[iz])
                 qc.ry(alpha[ia+1], qubits[iz+1])
                 ia += 2
 
-    
     def get_circuit(self):
         return self.qc
 
 
-    
 class LocalProjector:
-    
+
     def __init__(self, lcu, ansatz, Ub):
         self.Ub = Ub
         self.lcu = lcu
         self.ansatz = ansatz
         self.control_reg = QuantumRegister(1, name='control')
-        self.working_reg = QuantumRegister(lcu.gates[0].num_qubits, name='working')
+        self.working_reg = QuantumRegister(
+            lcu.gates[0].num_qubits, name='working')
         self.num_working_qubits = self.working_reg.size
 
-        
     def get_circuit(self, mu, nu, j=0):
         control_reg = self.control_reg
         working_reg = self.working_reg
-        
+
         inv = '\dagger'
         A_mu = self.lcu.gates[mu].control(1, label=f'$A_{mu}$')
-        A_nu = self.lcu.gates[nu].inverse().control(1, 
+        A_nu = self.lcu.gates[nu].inverse().control(1,
                                                     label=f'$A^{inv:s}_{nu:d}$')
-        
+
         num_qubits = A_mu.num_qubits
-       
+
         qc = QuantumCircuit(control_reg, working_reg, name='$\delta_{ij}$')
-        
+
         qc.append(self.ansatz.get_circuit(), working_reg)
-        qc.append(A_mu, 
-                  [control_reg, *working_reg])
+        qc.append(A_mu, [control_reg, *working_reg])
         if j > 0:
             qc.append(self.Ub.inverse(), working_reg)
             qc.cz(control_reg, j)
             qc.append(self.Ub, working_reg)
         qc.append(A_nu, [control_reg, *working_reg])
-        
+
         return qc
 
- 
-   
+
 class HadamardTest:
     """Hadamard Test class for expectation values
     
     Input:
         operator: must be already controlled!!
     """
-    
+
     def __init__(self, operator, imaginary=False, measure=False):
         self.operator = operator
         self.qc = None
         self.construct_circuit(imaginary=imaginary, measure=measure)
 
     def construct_circuit(self, imaginary=False, measure=False):
-        
+
         num_working_qubits = self.operator.num_qubits - 1
-       
+
         control_reg = QuantumRegister(1, name='control')
         working_reg = QuantumRegister(num_working_qubits, name='working')
-        
+
         if not measure:
             qc = QuantumCircuit(control_reg, working_reg)
         else:
-            
+
             classical_reg = ClassicalRegister(1)
             qc = QuantumCircuit(control_reg, working_reg, classical_reg)
-        
+
         qc.h(control_reg)
         if imaginary:
             qc.sdg(control_reg)
@@ -265,9 +133,171 @@ class HadamardTest:
             qc.measure(control_reg, classical_reg)
         # draw(qc.decompose())
         self.qc = qc
-    
+
     def get_circuit(self):
         return self.qc
+    
+
+class VQLS:
+
+    def __init__(self, A, b, projector=LocalProjector, ansatz=None, 
+                 backend=backend, num_shots=1):
+        self.matrix = A
+        self.target = b
+        self.lcu = LinearDecompositionOfUnitaries(A)
+        self.Ub = unitary_from_column_vector(b)
+        if ansatz is None:
+            self.ansatz = FixedAnsatz(states2qubits(A.shape[0]))
+        else:
+            self.ansatz = ansatz
+            
+        self.projector = projector(self.lcu, self.ansatz, self.Ub)
+        self.num_working_qubits = self.projector.num_working_qubits
+        self.backend = backend
+        self.num_shots = num_shots
+        self.circuits = None
+        self.num_unitaries = None
+        self.num_jobs = None
+        self.solution = None
+        self.construct_circuits()
+
+    def construct_circuits(self):
+        num_qubits = self.num_working_qubits + 1
+        num_unitaries = self.lcu.num_unitaries
+        circuits = np.empty((num_qubits, num_unitaries, num_unitaries),
+                            dtype=object)
+        for j in range(num_qubits):
+            for m in range(num_unitaries):
+                for n in range(num_unitaries):
+                    circuits[j, m, n] = self.construct_term(m, n, j=j)
+
+        self.circuits = circuits
+        self.num_jobs = circuits.size*2
+        return self
+
+    def construct_term(self, mu, nu, j):
+        opt = 3
+
+        operation = self.projector.get_circuit(mu, nu, j=j)
+
+        hadamard_real = HadamardTest(operation, imaginary=False).get_circuit()
+        hadamard_imag = HadamardTest(operation, imaginary=True).get_circuit()
+
+        # transpiled = transpile([hadamard_real, hadamard_imag],
+        #                               backend=backend,
+        #                               optimization_level=opt)
+
+        # return transpiled
+        return (hadamard_real, hadamard_imag)
+
+    def run_circuits(self, values):
+        experiments = []
+
+        for pair in self.circuits.flatten().tolist():
+            for qc in pair:
+
+                experiments.append(qc.assign_parameters(values))
+        # binds = {p: v for (p, v) in zip(qc.parameters, values)}
+        self.job = execute(experiments,
+                           backend=self.backend)
+
+        return self
+
+    def get_results(self, between):
+        irange = range(between[0], between[1])
+
+        results = np.zeros(len(irange), dtype=float)
+        result = self.job.result()
+
+        if self.backend.name() == 'statevector_simulator':
+            for i in irange:  # 2 parts: real, imag per term
+                state = result.get_statevector(i)
+                results[i-between[0]] = state.probabilities([0])[0]
+        else:
+            for i in irange:  # 2 parts: real, imag per term
+                counts = result.get_counts(i)
+                results[i-between[0]] = counts['0']/self.num_shots
+
+        return results
+
+    def local_cost(self, values):
+        """Returns the normalization constant <psi|psi>, where |psi> = A |x>."""
+        num_working_qubits = self.num_working_qubits
+        coeffs = np.atleast_2d(self.lcu.coeffs)
+        L = coeffs.shape[1]
+
+        # Jobs
+        self.run_circuits(values)
+        num_jobs_beta = 2*L**2
+
+        # Beta results
+        index_jobs_beta = (0, num_jobs_beta)
+        beta_p0 = self.get_results(index_jobs_beta)
+        beta_p0 = 2*beta_p0 - 1
+        betas = beta_p0[:-1:2] + 1j*beta_p0[1::2]
+        betas = betas.reshape((L, L))
+
+        # delta results
+        num_jobs_delta = num_working_qubits*num_jobs_beta
+        index_jobs_delta = (num_jobs_beta, num_jobs_beta + num_jobs_delta)
+        delta_p0 = self.get_results(index_jobs_delta)
+        delta_p0 = 2*delta_p0 - 1
+        deltas = delta_p0[:-1:2] + 1j*delta_p0[1::2]
+        deltas = deltas.reshape((num_working_qubits, L, L))
+
+        delta_sum = coeffs.dot( deltas.sum(axis=0)
+                               ).dot(coeffs.conj().T).real[0, 0]
+        beta_norm = coeffs.dot(betas).dot(coeffs.conj().T).real[0, 0]
+
+        self.cost = 1/2 - delta_sum/beta_norm/num_working_qubits/2
+        return self.cost
+
+    def optimal_state(self, values_opt):
+        backend = Aer.get_backend('statevector_simulator')
+        qc = self.ansatz.get_circuit().assign_parameters(values_opt)
+        job = execute(qc, optimization_level=3,
+                      backend=backend)
+
+        state = job.result().get_statevector()
+
+        return np.asarray(state).real
+
+    def fun_callback(self, x):
+        print("{:.5e}".format(self.cost))
+        
+    
+    def solve(self, optimizer=None, **kwargs):
+        if optimizer is None:
+            try:
+                optimizer = self.optimizer
+            except:
+                raise ValueError("No optimizer")
+
+        parameters0 = np.random.rand(self.ansatz.num_parameters)
+        print("# Optimizing")
+        result = minimize(self.local_cost,
+                    method=optimizer,
+                    x0=parameters0,
+                    callback=self.fun_callback,
+                    **kwargs)
+        print("# End")
+
+        self.solution = self.optimal_state(result.x)
+        return self
+    
+    def get_solution(self, scaled=False):
+        x = self.solution
+        if scaled:
+            b_up_to_constant = x.dot(self.matrix)
+            constant = np.mean(self.target/b_up_to_constant)
+            xopt = constant*x
+        else:
+            xopt = x
+            
+        print("# approx", xopt)
+        
+        return xopt
+
 
 
 if __name__ == '__main__':

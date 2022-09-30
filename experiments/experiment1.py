d7dc3e278241086b9c5ea1b3357badddc575521b
archer <gitlab.in2mf@simplelogin.co>
2022-09-30 10:06:33 +0300

    added __init__.py, changed vqls to solvers

---
+++ experiments/experiment1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 29 15:38:15 2022
+
+@author: archer
+"""
+
+import numpy as np
+from qiskit import Aer
+from qlib.solvers.vqls import VQLS
+from scipy.io import loadmat
+
+
+
+backend = Aer.get_backend('statevector_simulator',
+                         max_parallel_threads=4,
+                         max_parallel_experiments=4,
+                         precision="single")
+
+# backend = qiskit.Aer.get_backend('qasm_simulator',
+#                                     max_parallel_threads=8,
+#                                     max_parallel_experiments=16,
+#                                     precision="single")
+size = 2**2
+num_layers = 3
+num_shots = 2**11
+tol = 1e-9
+
+filepath = "/home/archer/code/quantum/data/"
+matrices = loadmat(filepath + "stiffnessMatricesDataTraining.mat")\
+    ['stiffnessMatricesData'].transpose(2, 0, 1).astype(np.float64)
+solutions = loadmat(filepath + "solutionDataTraining.mat")['solutionData']
+
+b = np.zeros((16,))
+b[7] = -100
+b[14] = 100 
+
+
+
+options = {'maxiter': 50,
+           'tol': 1e-10,
+    'disp': True}
+
+# bounds = [(0, 2*np.pi) for _ in range(vqls.ansatz.num_parameters)]
+
+for A in matrices:
+    x = np.linalg.solve(A, b)
+    vqls = VQLS(A, b)
+
+
+    xa = vqls.solve(optimizer='COBYLA',  
+                          options=options).get_solution(scaled=True)
+    
+    ba = xa.dot(A)
+    
---
+++ qlib/solvers/test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Tue Jul  5 14:56:15 2022
+
+@author: archer
+"""
+import numpy as np
+from qiskit import Aer, QuantumCircuit
+from qiskit.extensions import UnitaryGate
+from qlib.utils import unitary_from_hermitian, linear_decomposition_of_unitaries
+from scipy.io import loadmat
+
+backend = Aer.get_backend('statevector_simulator',
+                         max_parallel_threads=4,
+                         max_parallel_experiments=4,
+                         precision="single")
+
+# backend = qiskit.Aer.get_backend('qasm_simulator',
+#                                     max_parallel_threads=8,
+#                                     max_parallel_experiments=16,
+#                                     precision="single")
+num_qubits = 4
+size = 2**num_qubits
+num_layers = 3
+num_shots = 2**11
+tol = 1e-8
+
+b = np.ones(size)
+A = np.random.rand(size, size)
+# A[ np.abs(A) < tol ] = 0
+
+
+
+
+
+A = 0.5*(A + A.conj().T) / np.linalg.norm(A)
+
+
+filepath = "/home/archer/code/quantum/data/"
+matrices = loadmat(filepath + "stiffnessMatricesDataTraining.mat")\
+    ['stiffnessMatricesData'].transpose(2, 0, 1).astype(np.float64)
+    
+A = matrices[0]
+x = np.linalg.solve(A, b)
+
+
+
+(F1, F2), c = linear_decomposition_of_unitaries(A)
+
+g = F1 @ F1.conj().T
+
+U = UnitaryGate(F1, label="why qiskit?")
+
+qc = QuantumCircuit(num_qubits + 1)
+
+qc.append(U, range(num_qubits))
+
+cu = U.control(1)
+
+qc.append(cu, range(num_qubits + 1))
+
+print(qc)

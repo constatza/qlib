e25b1bb6a763985b434a0fb2dd95b7d7b19773b2
archer <gitlab.in2mf@simplelogin.co>
2022-09-30 15:28:59 +0300

    added qiskit config

--- qlib/solvers/test.py
+++ qlib/solvers/test.py
@@ -9,8 +9,8 @@ Created on Tue Jul  5 14:56:15 2022
 import numpy as np
 from qiskit import Aer, QuantumCircuit
 from qiskit.extensions import UnitaryGate
-from qlib.utils import unitary_from_hermitian, linear_decomposition_of_unitaries,\
-    LinearDecompositionOfUnitaries
+from qlib.utils import states2qubits
+from qlib.solvers.vqls import VQLS
 from scipy.io import loadmat
 
 backend = Aer.get_backend('statevector_simulator',
@@ -22,47 +22,33 @@ backend = Aer.get_backend('statevector_simulator',
 #                                     max_parallel_threads=8,
 #                                     max_parallel_experiments=16,
 #                                     precision="single")
-num_qubits = 4
+num_qubits = 2
 size = 2**num_qubits
 num_layers = 3
 num_shots = 2**11
 tol = 1e-8
+np.random.seed(1)
 
 b = np.ones(size)
 A = np.random.rand(size, size)
 # A[ np.abs(A) < tol ] = 0
 
 
+options = {'maxiter': 50,
+           'tol': 1e-10,
+    'disp': True}
+
+# bounds = [(0, 2*np.pi) for _ in range(vqls.ansatz.num_parameters)]
 
 
-
-
-
-
-filepath = "/home/archer/code/quantum/data/"
-matrices = loadmat(filepath + "stiffnessMatricesDataTraining.mat")\
-    ['stiffnessMatricesData'].transpose(2, 0, 1).astype(np.float64)
-    
-A = matrices[np.random.randint(0, 999)]
 A = 0.5*(A + A.conj().T) 
 x = np.linalg.solve(A, b)
 
 
+x = np.linalg.solve(A, b)
+vqls = VQLS(A, b)
 
+xa = vqls.solve(optimizer='COBYLA',  
+                      options=options).get_solution(scaled=True)
 
-lcu = LinearDecompositionOfUnitaries(A)
-print(lcu.valid_decomposition())
-F1 = lcu.decomposition[0]
-g = F1 @ F1.conj().T
-
-U = UnitaryGate(F1, label="why qiskit?")
-
-qc = QuantumCircuit(num_qubits + 1)
-
-qc.append(U, range(num_qubits))
-
-cu = U.control(1)
-
-qc.append(cu, range(num_qubits + 1))
-
-print(qc)
+ba = xa.dot(A)

c4ae8a270bf7a3e5ba57751dd41cb329e692e978
night-archer <git.jinro@simplelogin.com>
2022-12-01 17:32:51 +0200

    Changed     Experiment         - FileLogger was moved inside Experiment class         - changed __init__ signature     FileLogger

--- experiments/custom4x4/experiment-surrogate.py
+++ experiments/custom4x4/experiment-surrogate.py
@@ -9,18 +9,16 @@ Created on Tue Nov 22 10:25:47 2022
 import os
 import numpy as np
 from keras.models import load_model
-
-from qlib.solvers.vqls import VQLS, FixedAnsatz, Experiment, RealAmplitudesAnsatz, \
-    SolutionPredictorSurrogate
-from qiskit.algorithms.optimizers import COBYLA, POWELL
-from qiskit.circuit.library import RealAmplitudes
-from qlib.utils import states2qubits, FileLogger
 from qiskit import Aer
 from scipy.optimize import basinhopping
+from qiskit.circuit.library import RealAmplitudes
+
+from qlib.utils import states2qubits, FileLogger
+from qlib.solvers.vqls import VQLS, FixedAnsatz, Experiment, RealAmplitudesAnsatz, \
+    SolutionPredictorSurrogate
 
 
-initial_parameter_provider = 'surrogate'
-outname = 'ml'
+initial_parameter_provider = 'mlp'
 input_path_physical_parameters = os.path.join("input", "parameters.in")
 
 parameters = np.loadtxt(input_path_physical_parameters)
@@ -28,74 +26,42 @@ parameters = np.loadtxt(input_path_physical_parameters)
 x = parameters[:, 0]
 y = parameters[:, 1]
 
-
 matrices = np.array([[-0.5*x**2, x*y],
                      [x*y, 2*y**2 + 1]])
-
-
-backend = Aer.get_backend('statevector_simulator',
-                        device='gpu',
-                          max_parallel_experiments=12,
-                          precision="single")
 
 matrices = matrices.transpose(2, 0, 1)
 
 matrices = np.block([[3*matrices, 2*matrices],
-                      [2*matrices, 4*matrices]])
-
+                     [2*matrices, 4*matrices]])
 
 N = matrices.shape[2]
-num_samples = matrices.shape[0]
-num_qubits = states2qubits(N)
-
-
 rhs = np.zeros((N,))
 rhs[0] = 1
 
+num_qubits = states2qubits(N)
+num_samples = matrices.shape[0]
+
+
 ansatz = FixedAnsatz(num_qubits=num_qubits, num_layers=1,
                      max_parameters=3)
-
-num_parameters = ansatz.num_parameters
-qc = ansatz.get_circuit()
-
-
-vqls = VQLS(
-            ansatz=ansatz,
-            backend=backend
-            )
-
-
 
 optimizer = 'bfgs'
 optimization_options = {'tol': 1e-4}
 
 model = load_model('model0')
 
-if initial_parameter_provider=='surrogate':
-    predictor = SolutionPredictorSurrogate(model, parameters, training_size=int(0.2*num_samples))
+if initial_parameter_provider == 'mlp':
+    predictor = SolutionPredictorSurrogate(
+        model, parameters, training_size=int(0.2*num_samples))
 elif initial_parameter_provider is None:
     predictor = None
 
-experiment = Experiment(matrices, rhs,
-                  optimizer=optimizer,
-                 solver=vqls,
-                   initial_parameter_predictor=predictor,
-                 backend=backend)
+experiment = Experiment(matrices, rhs, ansatz,
+                        optimizer=optimizer,
+                        initial_parameter_predictor=predictor,
+                        save_path='output/'+ initial_parameter_provider,
+                        dateit=True,
+)
 
 
-logger = FileLogger([name for name in experiment.data.keys()],
-                    subdir=outname,
-                    dateit=False)
-
-
-experiment.run(
-    logger=logger,
-    **optimization_options
-               )
-
-
-
-
-#
-# cobyla 18s
-# powell 70s
+experiment.run(**optimization_options)

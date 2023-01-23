0db40dbd9f0da87a05cafb2ad4ab50ec82b30301
cluster1 <cluster1>
2023-01-23 10:55:07 +0200

    experiment pauli

--- experiments/pauli/experiment_win.py
+++ experiments/pauli/experiment_win.py
@@ -35,25 +35,26 @@ z = zz.ravel()
 parameter_save_path = os.path.join('input',
                                    f'parameters-num_qubits_{NUM_QUBITS:d}')
 
-# np.save(parameter_save_path, np.vstack([x, y, z]).T)
+parameters = np.vstack([x, y, z]).T
+# np.save(parameter_save_path, parameters)
 
 if NUM_QUBITS == 3:
-    parameters = np.array([[x**3, x**2*y, x*y**2],
+    parameters_matrix = np.array([[x**3, x**2*y, x*y**2],
                            [z*x**3, x*y, z**2 + x],
                            [z+y**2, z*x + y, y**3 + x]])
 elif NUM_QUBITS == 2:
-    parameters = np.array([[np.cos(x + y), np.sin(y)],
+    parameters_matrix = np.array([[np.cos(x + y), np.sin(y)],
                            [-np.sin(z), np.cos(x**2 + z)]])
 
-parameters = 1/2*(parameters + parameters.transpose(1, 0, 2))
+parameters_matrix = 1/2*(parameters_matrix + parameters_matrix.transpose(1, 0, 2))
 
 eye = Operator(np.eye(N))
 XZ = Operator(Pauli('XZ'))
 
 matrices = []
-for n in range(parameters.shape[-1]):
+for n in range(parameters_matrix.shape[-1]):
     op = eye
-    params = parameters[:, :, n]
+    params = parameters_matrix[:, :, n]
     norm = np.linalg.norm(params)
     for i in range(NUM_QUBITS):
         for j in range(NUM_QUBITS):
@@ -65,7 +66,7 @@ for n in range(parameters.shape[-1]):
 
 matrices = np.array(matrices)
 
-num_samples = matrices.shape[0]
+num_samples = parameters.shape[0]
 rhs = np.zeros((N,))
 rhs[0] = 1
 
@@ -76,11 +77,11 @@ ansatz = FixedAnsatz(num_qubits=NUM_QUBITS,
 
 
 if INITIAL_PARAMETER_PROVIDER == 'mlp':
-    model = load_model(os.path.join('models', 'mlp'))
+    model = load_model(os.path.join("C:", '\\Users', 'cluster', 'constantinos', 'quantum', 'experiments', 'pauli', 'models', 'mlp'))
     predictor = SolutionPredictorSurrogate(
         model,
         parameters,
-        training_size=int(0.2*num_samples),
+        training_size=int(0.1*num_samples),
     )
 elif INITIAL_PARAMETER_PROVIDER is None:
     predictor = None
@@ -88,7 +89,7 @@ elif INITIAL_PARAMETER_PROVIDER is None:
 experiment = Experiment(matrices, rhs, ansatz,
                         optimizer=OPTIMIZER,
                         initial_parameter_predictor=predictor,
-                        save_path=f'output/num-qubits-{NUM_QUBITS:d}_',
+                        save_path=r'.\output\num_qubits_',
                         dateit=True,
                         backend=backend,
                         )
